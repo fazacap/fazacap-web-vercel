1567aadcd0165a125bba2999107ab43f96438a6a
FazaCap <98866891+fazacap@users.noreply.github.com>
2023-08-01 21:18:21 +0700

    Add files via upload

---
+++ assets/QCardSection.219daa2e.js
@@ -0,0 +1,1 @@
+import{c as r,h as e}from"./use-dark.51f55d3d.js";import{c as n,h as c}from"./index.75c1813e.js";var l=r({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:o}){const a=n(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>c(t.tag,{class:a.value},e(o.default))}});export{l as Q};
---
+++ assets/QSpace.11c8d5cf.js
@@ -0,0 +1,1 @@
+import{c as e}from"./use-dark.51f55d3d.js";import{h as a}from"./index.75c1813e.js";const p=a("div",{class:"q-space"});var o=e({name:"QSpace",setup(){return()=>p}});export{o as Q};

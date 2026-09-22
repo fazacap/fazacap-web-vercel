ecae495d481c35c1d4f49637383b2d65b33bcb49
FazaCap <98866891+fazacap@users.noreply.github.com>
2026-09-22 23:25:46 +0700

    Add files via upload

---
+++ assets/use-timeout-ChDbEomR.js
@@ -0,0 +1,1 @@
+import{a as u}from"./vm-qDR9utLm.js";import{Z as i,q as m,g as s}from"./index-DbGWidym.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
